74a3dd3a90921f34f075096b8a358eb20373f255
DMU-DEMON <demonop1769@gmail.com>
2025-06-28 17:02:04 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// Entry animation
-document.addEventListener("DOMContentLoaded", () => {
-  const body = document.body;
-  body.classList.add("transition", "duration-700", "ease-in-out", "opacity-0");
-  setTimeout(() => body.classList.remove("opacity-0"), 50);
-});
